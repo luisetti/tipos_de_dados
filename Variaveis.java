3f52060987935a02c552bcac396ecc405849da39
luisetti <luis.s.silvafernando@gmail.com>
2023-02-08 16:50:17 +0000

    Variaveis

---
+++ Variaveis.java
@@ -0,0 +1,23 @@
+public class Variaveis{
+
+     public static void main(String[] args){
+
+        int idade=20;
+        byte quantidadeBola=5;
+        char letra='M';
+        boolean vivo=false;
+        double salario = 1340.50;
+        float salarioMenor = 1200.00f;
+        short alunosPalmital = 10000;
+        long populacaoPalmital = 22000;
+        System.out.println(idade);
+        System.out.println(quantidadeBola);
+        System.out.println(vivo);
+        System.out.println(salario);
+        System.out.println(salarioMenor);
+        System.out.println(alunosPalmital);
+        System.out.println(populacaoPalmital);
+        System.out.println(letra);
+
+
+}}
