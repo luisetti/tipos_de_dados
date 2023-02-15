645faf3b71486cc6660982c884fb01c560c5f79d
luisetti <luis.s.silvafernando@gmail.com>
2023-02-15 17:11:27 +0000

    salvar

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,36 @@
+/* 
 public class EstruturaDeRepeticao {
-    
+  
+    public static void main(String args[]){
+        int soma=0;
+        for(int i=1; i<1001; soma=i+soma){
+            System.out.println(soma);
+        }
+    }
 }
+*/
+
+/*
+public class EstruturaDeRepeticao {
+  
+    public static void main(String args[]){
+
+        for(int i=3; i<=100; i+=3){
+            System.out.println(i);
+        }
+    }
+}
+ */
+
+ /*
+ public class EstruturaDeRepeticao {
+  
+    public static void main(String args[]){
+        int fat=1;
+        for(int i=1; i<=10; i++){
+            fat *= i;
+            System.out.println(fat);
+        }
+    }
+}
+ */
